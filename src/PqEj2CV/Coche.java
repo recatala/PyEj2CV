865da9965f7e5ac4e35165b0f3e5ff0814dd2b82
pablo <pablo@pablo-PC>
2016-01-14 20:53:41 +0100

    Nuevo commit a dia 14 de enero de 2016: inicios

---
+++ src/PqEj2CV/Coche.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PqEj2CV;
+
+/**
+ *
+ * @author pablo
+ */
+public class Coche 
+{
+    String modelo;
+    String color;
+    boolean esMetalizado; 
+    String matricula;
+    int anyoFabricacion;
+}
